ca03c65b72076d23931cbc66c76a4421856d9515
Igor Ribeiro <igor_1917@hotmail.com>
2020-05-24 19:29:47 -0300

    finalização do modulo de primeiro projeto node e typescript

--- src/middlewares/ensureAuthiticated.ts
+++ src/middlewares/ensureAuthiticated.ts
@@ -2,6 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
+interface TokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
 export default function ensureAuthenticated(
     request: Request,
     response: Response,
@@ -11,7 +19,7 @@ export default function ensureAuthenticated(
     const authHeader = request.headers.authorization;
 
     if (!authHeader) {
-        throw new Error('JWT token is missing');
+        throw new AppError('JWT token is missing', 401);
     }
 
     const [, token] = authHeader.split(' ');
@@ -19,10 +27,14 @@ export default function ensureAuthenticated(
     try {
         const decoded = verify(token, authConfig.jwt.secret);
 
-        console.log(decoded);
+        const { sub } = decoded as TokenPayload;
+
+        request.user = {
+            id: sub,
+        };
 
         return next();
     } catch (err) {
-        throw new Error('Invalid JWT token');
+        throw new AppError('Invalid JWT token', 401);
     }
 }
