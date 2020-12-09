bf8bfab6cc69c247c6f5fa2c1301aa4c11ef4546
igorWebMASTER <igor_1917@hotmail.com>
2020-12-08 23:39:50 -0300

    testes de criação de usuário

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/en/configuration.html
- */
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig.json');
 
@@ -13,35 +9,30 @@ module.exports = {
     // bail: 0,
 
     // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "/private/var/folders/ln/sz_fcwyx15n7d_pcv_sdm2t80000gn/T/jest_dx",
+    // cacheDirectory: "C:\\Users\\danil\\AppData\\Local\\Temp\\jest",
 
     // Automatically clear mock calls and instances between every test
     clearMocks: true,
 
     // Indicates whether the coverage information should be collected while executing the test
-    // collectCoverage: false,
+    collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    // collectCoverageFrom: undefined,
+    collectCoverageFrom: ['<rootDir>/src/modules/**/services/*.ts'],
 
     // The directory where Jest should output its coverage files
-    // coverageDirectory: undefined,
+    coverageDirectory: 'coverage',
 
     // An array of regexp pattern strings used to skip coverage collection
     // coveragePathIgnorePatterns: [
-    //   "/node_modules/"
+    //   "\\\\node_modules\\\\"
     // ],
 
     // Indicates which provider should be used to instrument code for coverage
-    coverageProvider: 'v8',
+    // coverageProvider: "babel",
 
     // A list of reporter names that Jest uses when writing coverage reports
-    // coverageReporters: [
-    //   "json",
-    //   "text",
-    //   "lcov",
-    //   "clover"
-    // ],
+    coverageReporters: ['text-summary', 'lcov'],
 
     // An object that configures minimum threshold enforcement for coverage results
     // coverageThreshold: undefined,
@@ -154,7 +145,7 @@ module.exports = {
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     // testPathIgnorePatterns: [
-    //   "/node_modules/"
+    //   "\\\\node_modules\\\\"
     // ],
 
     // The regexp pattern or array of patterns that Jest uses to detect test files
@@ -177,8 +168,8 @@ module.exports = {
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     // transformIgnorePatterns: [
-    //   "/node_modules/",
-    //   "\\.pnp\\.[^\\/]+$"
+    //   "\\\\node_modules\\\\",
+    //   "\\.pnp\\.[^\\\\]+$"
     // ],
 
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,7 +1,9 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
-describe('CreateAppointment', async () => {
+describe('CreateAppointment', () => {
     it('should be able to create a new appointment', async () => {
         const fakeAppointmentsRepository = new FakeAppointmentRepository();
         const createAppointment = new CreateAppointmentService(
@@ -15,5 +17,26 @@ describe('CreateAppointment', async () => {
 
         expect(appointment).toHaveProperty('id');
         expect(appointment.provider_id).toBe('123123');
+    });
+
+    it('should not be able to create two appointments on the same date', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentDate = new Date(2020, 4, 10, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123123',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '123123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
