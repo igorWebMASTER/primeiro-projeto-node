e5b7f4b23205ad5c37e701a77bbaf356bff7eabe
igorWebMASTER <igor_1917@hotmail.com>
2021-01-20 23:42:40 -0300

    Finalizando testes

--- src/modules/users/services/ResetPasswordService.spec.ts
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -2,10 +2,12 @@ import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeUserTokensRepository from '../repositories/fakes/FakeUsersTokensRepository';
+
 import ResetPasswordService from './ResetPasswordService';
 
 let fakeUsersRepository: FakeUsersRepository;
 let fakeUserTokensRepository: FakeUserTokensRepository;
+
 let resetPassword: ResetPasswordService;
 
 describe('SendForgotPasswordEmail', () => {
@@ -21,20 +23,25 @@ describe('SendForgotPasswordEmail', () => {
 
     it('should be able to reset the password', async () => {
         const user = await fakeUsersRepository.create({
-            name: 'Jonh Doe',
-            email: 'jhon@example.com',
+            name: 'John Doe',
+            email: 'johndoe@example.com',
             password: '123456',
         });
 
         const { token } = await fakeUserTokensRepository.generate(user.id);
 
         await resetPassword.execute({
-            password: '1234551',
+            password: '123123',
             token,
         });
 
         const updatedUser = await fakeUsersRepository.findById(user.id);
 
-        expect(updatedUser?.password).toBe('1234421');
+        expect(updatedUser?.password).toBe('123123');
     });
 });
+
+// hash
+// 2 horas de expiração
+// userToken inexistente
+// user inexistente
