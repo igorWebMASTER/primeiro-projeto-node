6fcbc55d839ed8b5aa5c8ca3e2f7f795692cf904
igorWebMASTER <igor_1917@hotmail.com>
2021-02-03 23:53:35 -0300

    implementando Ethereal email

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,9 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProviders/models/IStorageProvider';
 import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
 
-// import IMailProvider from './MailProvider/models/IMailProvider';
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
     'IStorageProvider',
     DiskStorageProvider,
+);
+
+container.registerSingleton<IMailProvider>(
+    'MailProvider',
+    EtherealMailProvider,
 );
