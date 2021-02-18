075ad2f7007037c9e3f71319d81714ed1060a4e7
igorWebMASTER <igor_1917@hotmail.com>
2021-02-17 23:43:55 -0300

    filtrando agendamentos por mes

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointments';
 import ICreateAppointmentDto from '../dtos/ICreateAppointmentsDTO';
+import IFinAllInMonthFromProviderDTO from '../dtos/IFinAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
     create({ date }: ICreateAppointmentDto): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>;
+    findAllInMonthFromProvider(
+        data: IFinAllInMonthFromProviderDTO,
+    ): Promise<Appointment[]>;
 }
