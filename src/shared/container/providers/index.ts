ae29361ab240e504fd3980e2b04fd0bf72b64ba0
igorWebMASTER <igor_1917@hotmail.com>
2021-03-01 23:42:37 -0300

    implementando AWS SES

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,9 +3,6 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProviders/models/IStorageProvider';
 import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
 
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
@@ -17,9 +14,4 @@ container.registerSingleton<IStorageProvider>(
 container.registerSingleton<IMailTemplateProvider>(
     'MailTemplateProvider',
     HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-    'MailProvider',
-    container.resolve(EtherealMailProvider),
 );
